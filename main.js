50decfa503bbe3f2f894c3c510e8e8ba1477f9fb
PatriceKalwira <patricekalwira@gmail.com>
2023-09-27 11:20:21 +0200

    addition

--- main.js
+++ main.js
@@ -1,16 +1,11 @@
 console.log('GitHub Main Javascript File')
 
-function Calculate(term1, operator, term2){
-}
+function Calculate(term1, operator, term2) {}
 
-function Add(param1, param2){
-}
+function Add(param1, param2) {}
 
-function Sub(param1, param2){
-}
+function Sub(param1, param2) {}
 
-function Mul(param1, param2){
-}
+function Mul(param1, param2) {}
 
-function Div(param1, param2){
-}
+function Div(param1, param2) {}
