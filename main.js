6918ea828526bf51d7ed7634be068245876d5c57
Larson-Dav <larsonkapita@gmail.com>
2023-09-27 11:11:00 +0200

    Up to Date By Team Member Teask to Main.js

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
 console.log('GitHub Main Javascript File')
 
 function Calculate(term1, operator, term2){
+    if(operator == '+'){
+        console.log(Add(term1, term2))
+    } else if(operator == '-'){
+        console.log(Sub(term1, term2))
+    } else if(operator == '*'){
+        console.log(Mul(term1, term2))
+    } else if(operator == '/'){
+        console.log(Div(term1, term2))
+    } else {
+        return 'Operator not recognized'
+    }
 }
 
 function Add(param1, param2){
